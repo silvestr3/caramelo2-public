71f696957bb04f9498a3670c0f697df8fe75eabb
silvestr3 <fellipe.silvestre@outlook.com>
2024-03-20 11:55:10 +0700

    feat: Integrate with backend authentication

--- src/util/AuthorizedFetch.ts
+++ src/util/AuthorizedFetch.ts
@@ -1,18 +1,25 @@
-//import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-//import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export const authorizedFetch = async (url: string, options: RequestInit) => {
-	// const session = await getServerSession(authOptions);
-	// const backendToken = session?.user.accessToken;
+	const session = await getServerSession(authOptions);
+	const backendToken = session?.user.accessToken;
 
-	// if (!session) {
-	// 	return;
-	// }
+	if (!session) {
+		return;
+	}
 
-	// let newHeaders = {
-	// 	...options.headers,
-	// 	Authorization: `Bearer ${backendToken}`,
-	// };
+	let newHeaders = {
+		...options.headers,
+		Authorization: `Bearer ${backendToken}`,
+	};
 
-	return fetch(url, { ...options });
+	return fetch(url, { ...options, headers: newHeaders }).then((res) => {
+		if (res.status === 401) {
+			redirect("/login");
+		} else {
+			return res;
+		}
+	});
 };
